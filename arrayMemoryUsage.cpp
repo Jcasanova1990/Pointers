c8f9bd4060ee33380b9a939f01a0cc7846e08f79
jcasanova1990 <jeremykcasanova@gmail.com>
2024-08-16 10:19:36 -0400

    fourth

---
+++ arrayMemoryUsage.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+char names[3][6] = {
+    "Alan", "Bob", "joe"
+};
+
+for (int i = 0; i < sizeof(names) / sizeof(names[0]); i++) {
+    cout << names[i] << endl;
+}
+
+return 0;
+
+}
